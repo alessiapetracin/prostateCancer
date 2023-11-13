919a577ab46b32202254d9300809b3c94c2d76df
Alessia Petracin <126952273+alessiapetracin@users.noreply.github.com>
2023-11-13 22:03:32 +0100

    Update decision_tree.R

--- decision_tree.R
+++ decision_tree.R
@@ -11,4 +11,37 @@ plot(tree.prostate) # plot the tree
 text(tree.prostate, pretty = 0) # remove variable names
 title(main = 'Prostate regression tree') # add title to the plot
 
+summary(tree.prostate) # check variables used in the regression tree
 
+# perform 10-fold cross-validation
+x <- as.numeric(list())
+
+for (i in 1:1000){
+set.seed(i)
+cv.prostate <- cv.tree(object=tree.prostate)
+lower <- cv.prostate$size[which.min(cv.prostate$dev)]
+x <- append(x, as.numeric(lower))
+}
+
+# plot the CV error as a function of size and cost-complexity of the tree, for the last iterated cross-validation
+op <- par(mfrow=c(1, 2))
+plot(cv.prostate$size, cv.prostate$dev, type="b")
+title(main = 'CV error by size')
+plot(cv.prostate$k, cv.prostate$dev, type="b")
+title(main = 'CV error by cost-complexity')
+par(op)
+
+# select size of the tree through the mode (value which gives the lowest error most frequently over 1000 iterations according to CV
+Mode <- function(x) {
+  ux <- unique(x)
+  ux[which.max(tabulate(match(x, ux)))]
+}
+
+complexity <- Mode(x)
+complexity
+
+# use this value to prune the tree
+prune.prostate <- prune.tree(tree.prostate, best = complexity)
+plot(prune.prostate) # plot the pruned tree
+text(prune.prostate, pretty = 0) # remove variable names
+title(main = 'Pruned regression tree') # add title
